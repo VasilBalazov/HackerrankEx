25e11fe7d8de6c4ab2a08307d8399b1cf1162774
v.balazov <vasilbalazov@gmail.com>
2022-07-30 23:54:23 +0300

    Long to Binary String

---
+++ HackerrankEx/FlippingBits.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result11 {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        String binary = Long.toBinaryString(n); //parse long into binary str
+        while(binary.length()!=32) //turn str into 32bit length
+            binary = '0' + binary;
+
+        binary = binary.replace("1","3");
+        binary = binary.replace("0","1");
+        binary = binary.replace("3","0");
+
+        return Long.parseLong(binary,2);
+    }
+
+}
+
+public class FlippingBits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result11.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
